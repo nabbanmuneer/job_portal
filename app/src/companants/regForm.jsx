0a04e387dea3f0d2680d313b0bec8fa71a3c9fae
Nabban muneer pp <nabbanmuneerpp@gmail.com>
2023-01-12 10:02:40 +0530

    form

--- app/src/companants/regForm.jsx
+++ app/src/companants/regForm.jsx
@@ -37,7 +37,7 @@ const regForm = () => {
                              type="password" placeholder=" Re-Password" aria-label="Re-Password" 
                         />
                     </div>
-                    <button classNameass="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+                    <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
                         Sign Up
                     </button>
                     <button className="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded" type="button">
